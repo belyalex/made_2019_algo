666fb2d4cd249834f6679e88db80ea991b447282
alexey <belyjav@c-i-systems.com>
2019-10-25 13:39:10 +0300

    Поправил код в соответствии с Минимальными правилами кодирования. Исправил баг в Stack::top().

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,3 +1,46 @@
+/*
+1_3. Очередь с помощью стека
+Ограничение времени	1 секунда
+Ограничение памяти	20Mb
+Ввод	стандартный ввод или input.txt
+Вывод	стандартный вывод или output.txt
+Реализовать очередь с помощью двух стеков. Использовать стек, реализованный с помощью динамического буфера.
+
+Обрабатывать команды push back и pop front.
+
+Формат ввода
+В первой строке количество команд n. n ≤ 1000000.
+
+Каждая команда задаётся как 2 целых числа: a b.
+
+a = 2 - pop front
+a = 3 - push back
+
+Если дана команда pop front, то число b - ожидаемое значение. Если команда pop front вызвана для пустой структуры данных, то ожидается “-1”.
+
+Формат вывода
+Требуется напечатать YES - если все ожидаемые значения совпали. Иначе, если хотя бы одно ожидание не оправдалось, то напечатать NO.
+
+Пример 1
+Ввод	Вывод
+3
+3 44
+3 50
+2 44
+YES
+Пример 2
+Ввод	Вывод
+2
+2 -1
+3 10
+YES
+Пример 3
+Ввод	Вывод
+2
+3 44
+2 66
+*/
+
 #include <assert.h>
 #include <iostream>
 #include <cstring>
@@ -15,6 +58,7 @@ private:
 	int* head = buffer;
 	int size = initial_size;
 public:
+	Stack() { assert(buffer != nullptr); };
 	~Stack() { if (buffer != nullptr) delete[] buffer; };
 };
 
@@ -24,7 +68,7 @@ bool Stack::empty() const {
 }
 int Stack::top() const {
 	assert(!empty());
-	return *head;
+	return *(head-1);
 }
 
 int Stack::pop() {
@@ -35,6 +79,7 @@ int Stack::pop() {
 void Stack::push(int value) {
 	if (buffer + size == head) {
 		int* new_buffer = new int[size * 2];
+		assert(new_buffer != nullptr);
 		std::memcpy(new_buffer, buffer, size*sizeof(int));
 		delete[] buffer;
 		buffer = new_buffer;
