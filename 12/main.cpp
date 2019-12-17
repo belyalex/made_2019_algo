48ab18b8b8d16e391f625a01fc2cd9154f994318
alexey <belyjav@c-i-systems.com>
2019-12-17 12:55:18 +0300

    Переделал на нерекурсивный обход

--- 12/main.cpp
+++ 12/main.cpp
@@ -1,40 +1,8 @@
-/*
-Ребро неориентированного графа называется мостом, если удаление этого ребра из графа 
-увеличивает число компонент связности.
-
-Дан неориентированный граф, требуется найти в нем все мосты.
-
-Формат ввода
-Первая строка входного файла содержит два целых числа n и m — количество вершин и ребер 
-графа соответственно (1 ≤ n ≤ 20000, 1 ≤ m ≤ 200000).
-
-Следующие m строк содержат описание ребер по одному на строке. Ребро номер i описывается 
-двумя натуральными числами bi, ei — номерами концов ребра (1 ≤ bi, ei ≤ n).
-
-Формат вывода
-Первая строка выходного файла должна содержать одно натуральное число b — количество 
-мостов в заданном графе. На следующей строке выведите b целых чисел — номера ребер, 
-которые являются мостами, в возрастающем порядке. Ребра нумеруются с единицы в том 
-порядке, в котором они заданы во входном файле.
-
-Пример
-Ввод
-6 7
-1 2
-2 3
-3 4
-1 3
-4 5
-4 6
-5 6
-
-Вывод
-1
-3
-*/
 #include <fstream>
 #include <vector>
+#include <stack>
 #include <algorithm>
+#include <tuple>
 
 class Graph {
 public:
@@ -43,32 +11,27 @@ public:
     std::vector<size_t> FindAllBridges();
 
     void AddEdge(size_t n, size_t x, size_t y);
-
 private:
     std::vector<std::vector<size_t>> vertexes; //для каждой вершины храним вектор смежных вершин
     std::vector<std::vector<size_t>> edges;    //для каждой вершины номера рёбер в том же порядке
 
-    std::vector<size_t> in_time;  //времена входа в вершины
-    std::vector<size_t> out_time; //времена выхода
-    std::vector<bool> visited;    //признаки посещения вершин
-
-    std::vector<size_t> bridges;  //сюда будем записывать номера рёбер - мостов
-    size_t time = 0;              //счётчик времени
-
-    void DFS(size_t v, size_t p = SIZE_MAX);
+    void DFS(size_t v, std::vector<bool>& visited, std::vector<size_t>& bridges,
+             std::vector<size_t>& in_time, std::vector<size_t>& out_time);
 };
 
 Graph::Graph(const size_t V) :
         vertexes(V),
-        edges(V),
-        visited(V, false),
-        in_time(V, 0),
-        out_time(V, 0) {}
+        edges(V) {}
 
 std::vector<size_t> Graph::FindAllBridges() {
+    std::vector<size_t> bridges; //сюда будем записывать номера рёбер - мостов
+    std::vector<bool> visited(vertexes.size(),false); //признаки посещения вершин
+    std::vector<size_t> in_time(vertexes.size(),0);   //времена входа в вершины
+    std::vector<size_t> out_time(vertexes.size(),0);  //времена выхода
+
     for (size_t v = 0; v < vertexes.size(); v++) {
         if (!visited[v]) {
-            DFS(v);
+            DFS(v, visited, bridges, in_time, out_time);
         }
     }
     std::sort(bridges.begin(), bridges.end());
@@ -85,28 +48,51 @@ void Graph::AddEdge(const size_t n, const size_t x, const size_t y) {
     edges[y].push_back(n);
 }
 
-// Обход графа в глубину
-// v - вершина в которую идём, p - номер ребра, по которому идём
-void Graph::DFS(const size_t v, const size_t p) {
-    visited[v] = true;
-    in_time[v] = out_time[v] = time++;
-    //цикл по всем смежным вершинам
-    for (size_t i = 0; i < vertexes[v].size(); i++) {
-        int w = vertexes[v][i];
-        int e = edges[v][i];
-        if (e == p) continue; //не рассматриваем то же ребро, по которому пришли в v
-        if (visited[w]) {
-            out_time[v] = std::min(out_time[v], in_time[w]);
-        } else {
-            DFS(w, e);
+// Нерекурсивный обход графа в глубину
+void Graph::DFS(const size_t v1, std::vector<bool>& visited,std::vector<size_t>& bridges,
+                std::vector<size_t>& in_time, std::vector<size_t>& out_time) {
+    size_t time = 0; //счётчик времени
+    std::stack<std::tuple<size_t, size_t, size_t>> stack; // в стеке храним вершину, номер смежной вершины и номер ребра
+    stack.push(std::make_tuple(v1,SIZE_MAX,0));
+    while (!stack.empty()) {
+next:
+        std::tuple<size_t,size_t,size_t> tuple=stack.top();
+
+        size_t v=std::get<0>(tuple);
+        size_t n=std::get<1>(tuple);
+        size_t p=std::get<2>(tuple);
+        if (!visited[v]) {
+            in_time[v] = out_time[v] = time++;
+            visited[v] = true;
+        }
+        for (size_t i = n + 1; i < vertexes[v].size(); i++) {
+            size_t w = vertexes[v][i];
+            size_t e = edges[v][i];
+            if (e == p) continue; //не рассматриваем то же ребро, по которому пришли в v
+            if (visited[w]) {
+                out_time[v] = std::min(out_time[v], in_time[w]);
+            } else {
+                std::get<1>(stack.top())=i;
+
+                stack.push(std::make_tuple(w,SIZE_MAX,e));
+                
+                goto next;
+            }
+        }
+        stack.pop();
+        if (!stack.empty()) {
+            size_t w = v;
+            v = std::get<0>(stack.top());
             out_time[v] = std::min(out_time[v], out_time[w]);
             if (out_time[w] > in_time[v]) {
                 // Нашли мост, добавляем в вектор мостов номер текущего ребра
-                bridges.push_back(e);
+                bridges.push_back(p);
             }
         }
+
     }
 }
+
 
 int main() {
     std::ifstream in("bridges.in");
